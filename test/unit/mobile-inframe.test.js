34503376590fb8210b40a27d3e9e805c9fd12441
sverosak <sveinung.rosaker@finn.no>
2013-11-08 11:00:39 +0100

    Added empty test with xit

---
+++ test/unit/mobile-inframe.test.js
@@ -0,0 +1,16 @@
+// var proxyquire = require('proxyquireify')(require);
+
+
+
+describe('Mobile inframe', function(){
+
+
+    xit('should support contextData plugin', function(){
+
+    });
+
+    xit('should apply requestAnimationFrame patch when ios matching ', function(){
+
+    });
+
+});
