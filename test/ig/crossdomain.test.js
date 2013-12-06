e9e3f6352804f3d34b0a90031af5597286961420
Gregers Rygg <gregersrygg@gmail.com>
2013-12-07 00:55:04 +0100

    Fix unstable test. Wasn't using runs() correctly: http://pivotal.github.io/jasmine/#section-Asynchronous_Support

--- test/ig/crossdomain.test.js
+++ test/ig/crossdomain.test.js
@@ -18,7 +18,6 @@ describe('Cross domain iframe test', function () {
 
         var name = 'cross_domain_test';
         var id = 'container_' + name;
-        var done = false;
 
         var body = document.getElementsByTagName('body')[0];
         var container = document.createElement('div');
@@ -26,46 +25,34 @@ describe('Cross domain iframe test', function () {
         container.id = id;
         body.appendChild(container);
 
+        var spy = jasmine.createSpy('done');
         manager.queue(name, {
             container: id,
             url: '/base/test/fixtures/config_content.js',
             width: 123,
             height: 321,
-            done: function (err, item) {
-                expect(err).toBeUndefined();
-                expect(item).toBeDefined();
-
-                if (item.input.height === 0) {
-                    console.debug('crossdomain.test.js failed test 1: debug info', item);
-                    if (item.com) {
-                        //TODO:
-                        console.log('re-checking');
-                        item.com({
-                            cmd: 'getSizes'
-                        });
-                    }
-                } else {
-                    done = true;
-                }
-
-                var src = item.iframe.element.getAttribute('src');
-
-                // as this will test in not usable browsers, we need to asure result
-
-                expect(src.indexOf(base) !== -1).toBe(true);
-            }
+            done: spy
         });
 
-        /*
-            This test failes sometimes in __ PhantomJS __ without wrapping setTimeout.
-            Not sure why, but as its only in phantom atm, its ok.
-        */
-        setTimeout(function () {
+        runs(function () {
             manager.render(name);
-        }, 100);
+        });
 
         waitsFor(function () {
-            return done;
+            return spy.calls.length===1;
+        });
+
+        runs(function () {
+            expect(spy).toHaveBeenCalled();
+            var err = spy.mostRecentCall.args[0];
+            var item = spy.mostRecentCall.args[1];
+            expect(err).toBeUndefined();
+            expect(item).toBeDefined();
+            expect(item.input.height).not.toEqual(0);
+            expect(item.input.height).toEqual(jasmine.any(Number));
+            
+            var src = item.iframe.element.getAttribute('src');
+            expect(src).toContain(base);
         });
 
     });
