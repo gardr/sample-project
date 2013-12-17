5a205f40371155c51d8d792fd9a75a5c17282018
Gregers Rygg <gregersrygg@gmail.com>
2013-12-17 10:46:12 +0100

    Collapse empty/failed banners

--- src/desktop.js
+++ src/desktop.js
@@ -1,7 +1,9 @@
 var getCookies = require('./lib/usercookies.js').getCookies;
+var insertCss = require('./lib/style/insertCss.js');
 var Manager = require('./lib/manager.js');
 module.exports = global.gardr = function (options) {
 	var manager = new Manager(options);
 	manager.extendInframeData(getCookies());
+	insertCss('.gardr-failed {display: none;}');
 	return manager;
 };
