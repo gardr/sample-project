497b206887fb0f14e0de09f0d00fdd4a081e5bb3
Gregers Rygg <gregersrygg@gmail.com>
2014-03-14 16:23:27 +0100

    The code under src/ is not in use anymore

--- src/desktop.js
+++
@@ -1,7 +0,0 @@
-var insertCss = require('./lib/style/insertCss.js');
-var Manager = require('./lib/manager.js');
-module.exports = global.gardr = function (options) {
-	var manager = new Manager(options);
-	insertCss('.gardr-failed {display: none;}');
-	return manager;
-};
--- src/lib/size.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(element) {
-    // TODO, get real size on element.
-    var size = element.getBoundingClientRect();
-
-    return {
-        width: Math.round(element.offsetWidth || size.width),
-        height: Math.round(element.offsetHeight || size.height)
-    };
-};
