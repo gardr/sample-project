eb0864a2068877a7fdf21bafaf4784f7d3bfa975
Gregers Rygg <gregersrygg@gmail.com>
2014-03-18 09:47:18 +0100

    Fix vendor-prefixes so the animation works in non-webkit browsers

--- public/banners/animation/index.js
+++ public/banners/animation/index.js
@@ -6,34 +6,74 @@ document.write('<style>\
             }\
             /* move the ball */\
             @-webkit-keyframes moveX {\
-          		from { \
-              		-webkit-transform: translateX(0);\
-              		-moz-transform: translateX(0);\
-              		-ms-transform: translateX(0);\
-              		-o-transform: translateX(0);\
-              		transform: translateX(0);\
-      			} to {\
-      				-webkit-transform: translateX(100%);\
-              		-moz-transform: translateX(100%);\
-              		-ms-transform: translateX(100%);\
-              		-o-transform: translateX(100%);\
-              		transform: translateX(100%);\
-  				}\
+                  from { \
+                      -webkit-transform: translateX(0);\
+                  } to {\
+                      -webkit-transform: translateX(100%);\
+                  }\
             }\
             @-webkit-keyframes moveY {\
                 from { \
-                	-webkit-transform: translateY(0);\
-              		-moz-transform: translateY(0);\
-              		-ms-transform: translateY(0);\
-              		-o-transform: translateY(0);\
-              		transform: translateY(0);\
-            	} to {\
-            		-webkit-transform: translateY(125px);\
-              		-moz-transform: translateY(125px);\
-              		-ms-transform: translateY(125px);\
-              		-o-transform: translateY(125px);\
-              		transform: translateY(125px);\
-            	}\
+                    -webkit-transform: translateY(0);\
+                } to {\
+                    -webkit-transform: translateY(125px);\
+                }\
+            }\
+            @-moz-keyframes moveX {\
+                  from { \
+                      -moz-transform: translateX(0);\
+                  } to {\
+                      -moz-transform: translateX(100%);\
+                  }\
+            }\
+            @-moz-keyframes moveY {\
+                from { \
+                      -moz-transform: translateY(0);\
+                } to {\
+                      -moz-transform: translateY(125px);\
+                }\
+            }\
+            @-ms-keyframes moveX {\
+                  from { \
+                      -ms-transform: translateX(0);\
+                  } to {\
+                      -ms-transform: translateX(100%);\
+                  }\
+            }\
+            @-ms-keyframes moveY {\
+                from { \
+                      -ms-transform: translateY(0);\
+                } to {\
+                      -ms-transform: translateY(125px);\
+                }\
+            }\
+            @-o-keyframes moveX {\
+                  from { \
+                      -o-transform: translateX(0);\
+                  } to {\
+                      -o-transform: translateX(100%);\
+                  }\
+            }\
+            @-o-keyframes moveY {\
+                from { \
+                      -o-transform: translateY(0);\
+                } to {\
+                      -o-transform: translateY(125px);\
+                }\
+            }\
+            @keyframes moveX {\
+                  from { \
+                      transform: translateX(0);\
+                  } to {\
+                      transform: translateX(100%);\
+                  }\
+            }\
+            @keyframes moveY {\
+                from { \
+                      transform: translateY(0);\
+                } to {\
+                      transform: translateY(125px);\
+                }\
             }\
             .circle {\
                 border-radius: 50%;\
@@ -46,12 +86,14 @@ document.write('<style>\
             	margin-right: 100px;\
                 -webkit-animation: moveX 5s linear 0s infinite alternate;\
                 -moz-animation: moveX 5s linear 0s infinite alternate;\
+                -ms-animation: moveX 5s linear 0s infinite alternate;\
                 -o-animation: moveX 5s linear 0s infinite alternate;\
                 animation: moveX 5s linear 0s infinite alternate;\
             }\
             .moveY {\
                 -webkit-animation: moveY 2s linear 0s infinite alternate;\
                 -moz-animation: moveY 2s linear 0s infinite alternate;\
+                -ms-animation: moveY 2s linear 0s infinite alternate;\
                 -o-animation: moveY 2s linear 0s infinite alternate;\
                 animation: moveY 2s linear 0s infinite alternate;\
             }\
